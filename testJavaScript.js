92058c649fec0a75b204c8dbf0333aa5de465925
BryLegHome <Brylegrain@gmail.com>
2022-02-18 11:58:21 +0100

    corrections in testJavaScript.js

--- testJavaScript.js
+++ testJavaScript.js
@@ -1,12 +1,18 @@
 
 const transform = (string) => {
     const vowels = ["a", "e", "i", "o", "u", "y"]
-    vowels.map((letter) => {
-        if (string.includes(letter)) {
-            string = string.replaceAll(letter, `${letter}p${letter}`)
-        }
-    })
+    if (typeof (string) === "string") {
+        string = string.toLowerCase();
+        vowels.map((letter) => {
+            if (string.includes(letter)) {
+                string = string.replaceAll(letter, `${letter}p${letter}`)
+            }
+        })
+    } else {
+        return 'type a correct string pls'
+    }
+
     return string;
 };
 
-console.log(transform("bonjour"));
+console.log(transform("bOnjoUr"));
